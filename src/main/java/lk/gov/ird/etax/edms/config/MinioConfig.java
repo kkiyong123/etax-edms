debe2d729e14c4d67a126fdce1f2ea6b8e3974bb
kkiyong123 <98229313+kkiyong123@users.noreply.github.com>
2026-07-15 00:51:51 +0900

    Fix MinioClient SSL bypass for ODF S3

--- src/main/java/lk/gov/ird/etax/edms/config/MinioConfig.java
+++ src/main/java/lk/gov/ird/etax/edms/config/MinioConfig.java
@@ -1,27 +1,38 @@
 package lk.gov.ird.etax.edms.config;
-
 import io.minio.MinioClient;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
 @Configuration
 public class MinioConfig {
-
     @Value("${minio.endpoint}")
     private String endpoint;
-
     @Value("${minio.access-key}")
     private String accessKey;
-
     @Value("${minio.secret-key}")
     private String secretKey;
-
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient() throws Exception {
+        TrustManager[] trustAll = new TrustManager[]{
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                public void checkClientTrusted(X509Certificate[] c, String a) {}
+                public void checkServerTrusted(X509Certificate[] c, String a) {}
+            }
+        };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAll, new java.security.SecureRandom());
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+            .sslSocketFactory(sc.getSocketFactory(), (X509TrustManager) trustAll[0])
+            .hostnameVerifier((h, s) -> true)
+            .build();
         return MinioClient.builder()
             .endpoint(endpoint)
             .credentials(accessKey, secretKey)
+            .httpClient(httpClient)
             .build();
     }
 }
