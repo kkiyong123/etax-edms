b71a92f77f7731bba20fcd66d6df06a808c9501e
kkiyong123 <98229313+kkiyong123@users.noreply.github.com>
2026-07-14 22:28:04 +0900

    Add src/main/java/lk/gov/ird/etax/edms/EtaxEdmsApplication.java

---
+++ src/main/java/lk/gov/ird/etax/edms/EtaxEdmsApplication.java
@@ -0,0 +1,11 @@
+package lk.gov.ird.etax.edms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaxEdmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EtaxEdmsApplication.class, args);
+    }
+}
